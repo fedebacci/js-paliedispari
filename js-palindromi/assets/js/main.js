724ca782781dc8fd628cfac2f81cfed33eb60e3c
fedebacci <federicobacci1999@gmail.com>
2025-04-07 15:49:56 +0200

    svolgimento esercizio palindromo

---
+++ js-palindromi/assets/js/main.js
@@ -0,0 +1,28 @@
+const btnCheckIfPalindrome = document.getElementById('checkIfPalindrome');
+
+btnCheckIfPalindrome.addEventListener('click', function () {
+    const valueToCheck = document.getElementById('valueToCheck').value;
+    const isValuePalindrome = isPalindrome(valueToCheck);
+    const message = isValuePalindrome === true ? `"${valueToCheck}" è un palindromo!` : `"${valueToCheck}" non è un palindromo!`;
+    alert(message);
+});
+
+function isPalindrome(value) {
+    
+    value = value.toString().toLowerCase();
+    let reversedValue = "";
+
+    // console.debug("value", value);
+    
+    for (let i = 0; i <= value.length - 1; i ++) {
+        const currentLetter = value[i];
+        // console.debug("currentLetter", currentLetter);
+        
+        if (currentLetter !== " ") {
+            reversedValue = currentLetter + reversedValue;
+            // console.debug("reversedValue", reversedValue);
+        };
+    };
+
+    return value.replaceAll(' ', '') === reversedValue ? true : false;
+};
